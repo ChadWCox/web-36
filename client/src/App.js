fc3fd061275bbad16c1296418f7c9d3d2f2f450a
Chad Cox <chadcox@Chads-MacBook-Pro.local>
2020-12-03 12:11:00 -0700

    finish

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+
+
+function url(path) {
+  return process.env.NODE_ENV === 'development'
+  ? `http://localhost:5000${path}`
+  : path
+}
 
 function App() {
+  const [data, setData] = useState('')
+  useEffect(() => {
+    fetch(url('/api/data'))
+    .then(res => res.json())
+    .then(goods => setData(goods.data))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        Web 36 Rocks!!
+        {data}
       </header>
     </div>
   );
