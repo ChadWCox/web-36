f2d9a1bb9c23c16d729f6327a24aff1cc60e988d
Chad Cox <chadcox@Chads-MacBook-Pro.local>
2020-12-03 10:55:54 -0700

    ready

---
+++ index.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+const port = process.env.PORT || 4000
+const cors = require('cors')
+
+console.log('web 36 rocks')
+console.log(__dirname)
+console.log(process.env.USER)
+console.log(process.env.PORT)
+console.log(process.env.LADY)
+console.log(process.env.ALWAYS)
+console.log(process.env.FOO)
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use('/app,', (_, res) => {
+    res.json({ message: `web 36 is AWESOME!!`})
+})
+
+app.listen(port, () => {
+ console.log(`listening on ${port}`)
+})
+
