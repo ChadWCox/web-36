518c2bfacff2fc9805f945794efa8806cc44c48a
Chad Cox <chadcox@Chads-MacBook-Pro.local>
2020-12-03 11:11:39 -0700

    api

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/*,', (_, res) => {
+app.use('/api,', (_, res) => {
     res.json({ message: `web 36 is AWESOME!!`})
 })
 
